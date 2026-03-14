8eaef11382c476a89f67ceea0387f021f4f14314
Mradusha Sharma <mradushasharma@Mradushas-MacBook-Pro.local>
2026-03-14 16:31:29 +0530

    Populate Hero.tsx and update landing page to include it

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link';
 import Footer from '@/components/home/Footer';
+import HeroSection from '@/components/home/Hero';
 export default function Home() {
   return (
     <>
+      <HeroSection />
       <Footer />
     </>
   );
